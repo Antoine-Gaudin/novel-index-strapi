48d4f0580481b27a17158f59020b20bf0fe7158e
gaudin <agaudin76@gmail.com>
2025-03-14 23:47:01 +0100

    retrait temporaire de la pagination de 25

--- config/api.ts
+++ config/api.ts
@@ -1,7 +1,7 @@
 export default {
   rest: {
-    defaultLimit: 25,
-    maxLimit: 100,
+    defaultLimit: -1, // Supprime la limite de pagination
+    maxLimit: -1,     // Aucune restriction sur la récupération de données
     withCount: true,
   },
 };
